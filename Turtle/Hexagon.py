334c571944d9d1dead9838bb7659722c21750edc
Richa Singh <richa797926@gmail.com>
2024-09-18 12:36:42 +0530

    first commit

---
+++ Turtle/Hexagon.py
@@ -0,0 +1,7 @@
+from turtle import *
+s = getscreen()
+t = Turtle()
+for i in range(6):
+    t.fd(110)
+    t.lt(60)
+mainloop()
---
+++ Turtle/Pentagon.py
@@ -0,0 +1,7 @@
+from turtle import *
+s = getscreen()
+t = Turtle()
+for i in range(5):
+    t.fd(110)
+    t.lt(72)
+mainloop()
